fb8721d389b588f6398f6762a2bcefeec587a12a
Rotz-kirwa <eliudkirwa451@gmail.com>
2026-06-20 19:36:35 +0300

    fix: redirect Nitro output to .vercel/output for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
   },
   nitro: {
     preset: "vercel",
+    output: {
+      dir: ".vercel/output",
+      serverDir: ".vercel/output/functions/__server.func",
+      publicDir: ".vercel/output/static",
+    },
   },
 });
